6c90f64fb276b053bdeea5eac20bdbda4cbbf12f
Michelle Anderson <manderson0102@yahoo.com>
2026-07-12 20:42:18 -0400

    updating userid

--- app/users/route.js
+++ app/users/route.js
@@ -3,6 +3,6 @@ import { NextResponse } from "next/server";
 export function GET(request, context) {
     const userId = context.params.userId;
     return NextResponse.json({
-        userId
+        userid
     })
 }
