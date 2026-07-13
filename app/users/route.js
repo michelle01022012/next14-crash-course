65ef9b9023ccefce54276d20d07e16409e22af32
Michelle Anderson <manderson0102@yahoo.com>
2026-07-12 20:39:46 -0400

    fixing code spelling

--- app/users/route.js
+++ app/users/route.js
@@ -1,9 +1,8 @@
-imort { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+
 export function GET(request, context) {
     const userId = context.params.userId;
-
     return NextResponse.json({
-        userId 
+        userId
     })
-
 }
