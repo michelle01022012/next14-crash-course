cb4d68b60060fc33e6c5eb933699848ca69c4882
Michelle Anderson <manderson0102@yahoo.com>
2026-07-14 09:29:32 -0400

    added firestore updates

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +11,9 @@ const firebaseConfig = {
   projectId: "next14-cc-d7d38",
   storageBucket: "next14-cc-d7d38.firebasestorage.app",
   messagingSenderId: "124808819290",
-  appId: "1:124808819290:web:9012d431b64f9b2a951e63"
+  appId: "1:124808819290:web:6de14bdf5490cd72951e63"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
