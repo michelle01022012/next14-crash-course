b5bcdbf475c67ab31184a3c87f09754bea8b46ad
Michelle Anderson <manderson0102@yahoo.com>
2026-07-14 04:46:55 -0400

    getting up to date

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,5 +1,11 @@
+import React from 'react'
+import Navbar from '@/components/Navbar'
+import CreatePost from '@/components/CreatePost'
+
+
 function Page() {
-  return <div>This is the posts page</div>
+  return <div>
+    This is the posts page</div>
 }
 
 export default Page
